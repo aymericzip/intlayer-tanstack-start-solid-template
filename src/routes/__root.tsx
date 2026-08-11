0b1d9370778fd494bac3b6291eaa82842f68bf65
aymericzip <ay.pineau@gmail.com>
2026-08-11 15:44:02 -0600

    chore: update dependencies and remove Header component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,18 +3,17 @@ import {
   Outlet,
   Scripts,
   createRootRouteWithContext,
-  useParams,
+  getRouteApi,
 } from "@tanstack/solid-router";
 import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
 import { HydrationScript } from "solid-js/web";
 import { Suspense } from "solid-js";
 import { IntlayerProvider } from "solid-intlayer";
 import { defaultLocale, getHTMLTextDir } from "intlayer";
-import { Route as LocaleRoute } from "./{-$locale}/route";
-
-import Header from "../components/Header";
 
 import styleCss from "../styles.css?url";
+
+const localeRoute = getRouteApi("/{-$locale}");
 
 export const Route = createRootRouteWithContext()({
   head: () => ({
@@ -24,8 +23,7 @@ export const Route = createRootRouteWithContext()({
 });
 
 function RootComponent() {
-  // Try to find locale in params of any active match
-  const params = LocaleRoute.useParams();
+  const params = localeRoute.useParams();
   const locale = params()?.locale ?? defaultLocale;
 
   return (
@@ -37,7 +35,6 @@ function RootComponent() {
         <HeadContent />
         <IntlayerProvider locale={locale}>
           <Suspense>
-            <Header />
             <Outlet />
             <TanStackRouterDevtools />
           </Suspense>
